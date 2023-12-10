4d46a1852c506f2890d80607f79b6c028e5f9cd3
komatsu <simatotumugi@gmail.com>
2023-12-10 18:52:30 +0900

    Remove mysite directory

--- MyV_portfolio/mysite/myapp/tokens.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-
-class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        # Djangoの標準Userモデルを使っている場合は、次のようになります。
-        # return f"{user.pk}{timestamp}{user.is_active}"
-        return f"{user.pk}{timestamp}"
-
-account_activation_token = AccountActivationTokenGenerator()
